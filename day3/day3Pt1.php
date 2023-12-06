05632e0ad6f9d5c0ba0d4728cec87e248f61579c
kochq <matukoch@live.com>
2023-12-05 22:32:33 -0300

    Day 3 pt 1

---
+++ day3/day3Pt1.php
@@ -0,0 +1,53 @@
+<?php
+$file = file("input");
+
+$dir = [
+  [-1,-1], [0,-1], [1,-1],
+  [-1, 0],         [1, 0],
+  [-1, 1], [0, 1], [1, 1]
+];
+
+$sum = 0;
+
+function getAdjacent($i, $j, $coords, $file) {
+  [$x, $y] = $coords;
+  if($i+$x < 0 || $i+$x >= count($file)) return null;
+  if($j+$y < 0 || $j+$y >= strlen($file[$i+$x])) return null;
+  return $file[$i + $x][$j + $y];
+}
+
+for($i = 0; $i < count($file); $i++) {
+  $tempPartNumber = "";
+  $largerNumber = false;
+  $isPartNumber = false;
+  for($j = 0; $j < strlen($file[$i]); $j++) {
+    if(is_numeric($file[$i][$j])) {
+      // if we found a number, check if it's part of a larger number
+      if(!$largerNumber) $tempPartNumber .= $file[$i][$j];
+      $numberAdj = getAdjacent($i, $j, $dir[6], $file);
+      if(is_numeric($numberAdj)) {
+        $tempPartNumber .= $numberAdj;
+        $largerNumber = true;
+      }
+
+      // Then, check if it's adjacent it's a part number
+      for($k = 0; $k < count($dir); $k++) {
+        $adj = getAdjacent($i, $j, $dir[$k], $file);
+        if($adj != '.' && $adj != null && !is_numeric($adj) && $adj != "\n") {
+          $isPartNumber = true;
+        }
+      }
+    } 
+    else if($tempPartNumber != "") { // If we found a non-number, and we have a number, add it to the sum
+      if($isPartNumber) { // Add the number to the sum
+        $sum += intval($tempPartNumber); 
+      }
+      // Reset the flags
+      $largerNumber = false; 
+      $isPartNumber = false;
+      $tempPartNumber = ""; 
+    }
+  }
+}
+
+print "The sum is: $sum\n";
